dd59119fa2e1e09a75b2b85fd25fbd9b8eba773c
thobbsinteractive <thobbsinteractive@gmail.com>
2016-06-01 20:57:42 +0100

    Major fix to text alignment. Implementation of group box

--- JavaScriptOrganisationalChart/scripts/jorg-chart-1.0.js
+++ JavaScriptOrganisationalChart/scripts/jorg-chart-1.0.js
@@ -7,9 +7,9 @@ function JOrganisationChart(svgElement, chartData, x, y)
 
 	this.chartAlign = "centre";
 
-	this.nodeTitleSize = 30; //Default to 30px
+	this.nodeTitleSize = 40; //Default to 30px
 	this.nodeLineSpacing = 0;
-	this.nodeTextSize = 20; //Default to 20px
+	this.nodeTextSize = 30; //Default to 20px
 	this.nodePadding = 10; //Default to 10px
 	this.nodeMargin = 10; //Default to 10px
 	this.nodeFont = "Arial";
@@ -17,22 +17,43 @@ function JOrganisationChart(svgElement, chartData, x, y)
 	this.nodeBackGroundColour = "rgba(50,50,125,0.8)"
 	this.nodeBorderColour = "rgba(0,0,50,1)"
 	this.nodeTextColour = "rgba(0,0,0,1)"
+	this.groupBackGroundColour = "rgba(50,50,125,0.8)"
+	this.groupBorderColour = "rgba(0,0,50,1)"
+	this.groupTextColour = "rgba(0,0,0,1)"
 
 	if (chartData != undefined)
 	{
 		if (chartData.group != undefined)
 		{
-			if (chartData.group.nodes != undefined && chartData.group.nodes.length > 0) {
-				for(var i = 0; i < chartData.group.nodes.length; i++)
-				{
-				    this.drawNode(100, 100, this.svgElement, chartData.group.nodes[i]);
-				}
-			}
+		    this.drawGroup(200, 20, svgElement, chartData.group)
 		}
 	}
 }
 
-JOrganisationChart.prototype.calculateNodeSize = function (svgElement, nodeData) {
+JOrganisationChart.prototype.calculateGroupSize = function (group) {
+    var dimensions = { width: 0, height: 0 };
+
+    if (group != undefined) {
+        dimensions.width = this.nodeMargin;
+
+        if (group.nodes != undefined && group.nodes.length > 0) {
+            for (var i = 0; i < group.nodes.length; i++) {
+                var nodeDimensions = this.calculateNodeSize(group.nodes[i]);
+
+                dimensions.width = dimensions.width + nodeDimensions.width + this.nodeMargin;
+                if (nodeDimensions.height > dimensions.height)
+                {
+                    dimensions.height = nodeDimensions.height;
+                }
+            }
+            dimensions.height = dimensions.height + (this.nodeMargin*2);
+        }
+    }
+
+    return dimensions;
+}
+
+JOrganisationChart.prototype.calculateNodeSize = function (nodeData) {
 	var dimensions = { width: 0, height: 0 };
 
 	if (nodeData != undefined)
@@ -42,7 +63,7 @@ JOrganisationChart.prototype.calculateNodeSize = function (svgElement, nodeData)
 		//Measure Width (max line width + padding)
 		if (nodeData.title != undefined && nodeData.title.length > 0)
 		{
-		    dimensions.width = (nodeData.title.length * (this.nodeTitleSize/2)) + (this.nodePadding * 2);
+		    dimensions.width = this.calculateTextWidth(nodeData.title, this.nodeTitleSize);
 		    carrageLoc = carrageLoc + this.nodeTitleSize + this.nodeLineSpacing;
 		}
 
@@ -53,7 +74,7 @@ JOrganisationChart.prototype.calculateNodeSize = function (svgElement, nodeData)
 			for (var i = 0; i < nodeData.text.length;i++)
 			{
 				carrageLoc = carrageLoc + this.nodeTextSize;
-				lineWidth = (nodeData.text[i].length * (this.nodeTextSize / 2)) + (this.nodePadding * 2);
+				lineWidth = this.calculateTextWidth(nodeData.text,this.nodeTextSize);
 				if (dimensions.width < lineWidth)
 				{
 					dimensions.width = lineWidth;
@@ -64,29 +85,58 @@ JOrganisationChart.prototype.calculateNodeSize = function (svgElement, nodeData)
 				    carrageLoc = carrageLoc + this.nodeLineSpacing;
 				}
 			}
-			carrageLoc = carrageLoc + this.nodePadding;
 		}
-
+		carrageLoc = carrageLoc + this.nodePadding;
+		dimensions.width = dimensions.width + (this.nodePadding * 2);
 		dimensions.height = carrageLoc;
 	}
 
 	return dimensions;
 };
 
+JOrganisationChart.prototype.calculateTextWidth = function(text,fontSize)
+{
+    return text.length * (fontSize / 2);
+}
+
+JOrganisationChart.prototype.drawGroup = function (cx, cy, svgElement, group) {
+    if (group != undefined) {
+        var grpDimensions = this.calculateGroupSize(group)
+
+        //Draw group box
+        var groupBoxSVG = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        groupBoxSVG.setAttribute('x', cx - (grpDimensions.width / 2));
+        groupBoxSVG.setAttribute('y', cy);
+        groupBoxSVG.setAttribute('width', grpDimensions.width);
+        groupBoxSVG.setAttribute('height', grpDimensions.height);
+        groupBoxSVG.setAttribute("style", "fill:" + this.groupBackGroundColour + ";stroke:" + this.groupBorderColour + ";stroke-width:2;");
+
+        svgElement.append(groupBoxSVG);
+
+        var ncx = (cx - (grpDimensions.width / 2)) + this.nodeMargin;
+        if (group.nodes != undefined && group.nodes.length > 0) {
+            for (var i = 0; i < group.nodes.length; i++) {
+                var nodeDimensions = this.calculateNodeSize(group.nodes[i]);
+                ncx = ncx + (nodeDimensions.width / 2);
+                this.drawNode(ncx, cy + this.nodeMargin, this.svgElement, group.nodes[i]);
+                ncx = ncx + (nodeDimensions.width / 2) + this.nodeMargin;
+            }
+        }
+    }
+}
+
 JOrganisationChart.prototype.drawNode = function (cx, cy, svgElement, nodeData) {
 
     if (nodeData != undefined && svgElement != undefined)
 	{
-		var dimensions = this.calculateNodeSize(svgElement, nodeData);
-
-		var x = cx + (dimensions.width / 2);
+		var dimensions = this.calculateNodeSize(nodeData);
 
         //Draw node box
 		var nodeBoxSVG = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-		nodeBoxSVG.setAttribute('x', x);
+		nodeBoxSVG.setAttribute('x', cx - (dimensions.width / 2));
 		nodeBoxSVG.setAttribute('y', cy);
-		nodeBoxSVG.setAttribute('width', x);
-		nodeBoxSVG.setAttribute('height', cy);
+		nodeBoxSVG.setAttribute('width', dimensions.width);
+		nodeBoxSVG.setAttribute('height', dimensions.height);
 		nodeBoxSVG.setAttribute("style", "fill:" + this.nodeBackGroundColour + ";stroke:" + this.nodeBorderColour + ";stroke-width:2;");
 
 		svgElement.append(nodeBoxSVG);
@@ -95,10 +145,12 @@ JOrganisationChart.prototype.drawNode = function (cx, cy, svgElement, nodeData) 
 
 		if (nodeData.title != undefined && nodeData.title.length > 0) {
 
-		    carrageLoc = carrageLoc + this.nodeTitleSize;
+		    carrageLoc = carrageLoc + (this.nodeTitleSize * 0.75);
 		    //Render Title
 		    var nodeTitleSVG = document.createElementNS("http://www.w3.org/2000/svg", "text");
-		    nodeTitleSVG.setAttribute('x', x + this.nodePadding);
+		    nodeTitleSVG.setAttribute('text-anchor', 'middle');
+		    nodeTitleSVG.setAttribute('alignment-baseline', 'central');
+		    nodeTitleSVG.setAttribute('x', cx);
 		    nodeTitleSVG.setAttribute('y', carrageLoc);
 		    nodeTitleSVG.setAttribute("fill", this.nodeTextColour);
 		    nodeTitleSVG.setAttribute("style", "font-family:" + this.nodeFont + "; font-size:" + this.nodeTitleSize + "px;");
@@ -106,7 +158,7 @@ JOrganisationChart.prototype.drawNode = function (cx, cy, svgElement, nodeData) 
 
 		    svgElement.append(nodeTitleSVG);
 
-		    carrageLoc = carrageLoc + (this.nodeTitleSize / 2);
+		    carrageLoc = carrageLoc + (this.nodeTitleSize * 0.25);
 		    carrageLoc = carrageLoc + this.nodeLineSpacing;
 		}
 
@@ -114,17 +166,19 @@ JOrganisationChart.prototype.drawNode = function (cx, cy, svgElement, nodeData) 
         if (nodeData.text != undefined && nodeData.text.length > 0) {
             for (var i = 0; i < nodeData.text.length; i++) {
 
-                carrageLoc = carrageLoc + (this.nodeTextSize/2);
+                carrageLoc = carrageLoc + (this.nodeTextSize * 0.75);
 
                 var nodeTextSVG = document.createElementNS("http://www.w3.org/2000/svg", "text");
-                nodeTextSVG.setAttribute('x', x + this.nodePadding);
+                nodeTextSVG.setAttribute('text-anchor', 'middle');
+                nodeTextSVG.setAttribute('alignment-baseline', 'central');
+                nodeTextSVG.setAttribute('x', cx);
                 nodeTextSVG.setAttribute('y', carrageLoc);
                 nodeTextSVG.setAttribute("fill", this.nodeTextColour);
                 nodeTextSVG.setAttribute("style", "font-family:" + this.nodeFont + "; font-size:" + this.nodeTextSize + "px;");
                 nodeTextSVG.textContent = nodeData.text[i];
                 svgElement.append(nodeTextSVG);
 
-                carrageLoc = carrageLoc + (this.nodeTextSize / 2);
+                carrageLoc = carrageLoc + (this.nodeTextSize * 0.25);
 
                 if (i < (nodeData.text.length - 1)) {
                     carrageLoc = carrageLoc + this.nodeLineSpacing;
